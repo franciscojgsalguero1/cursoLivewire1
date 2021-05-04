fbfb984a62145242e1d8ff28116995c67c1fc25f
Francisco José Gordo Salguero <franciscojgsspam@gmail.com>
2021-05-04 17:59:49 +0200

    acabado el capítulo 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory([
+            'name' => 'prueba',
+            'email' => 'prueba@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$66gcyeipIojimGx6D9WKY.iYOfaUq3W1VHbykBgaHhoXMbwfErHNW', // password prueba12
+            'remember_token' => \Illuminate\Support\Str::random(10),
+            'current_team_id' => 1
+        ])->create();
+        \App\Models\Team::factory([
+            'user_id' => 1,
+            'name' => 'prueba\'s treams',
+            'personal_team' => true
+        ])->create();
+        
+        \App\Models\User::factory(10)->create();
+        \App\Models\Team::factory(100)->create();
+        \App\Models\Post::factory(100)->create();
     }
 }
